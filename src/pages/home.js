64db0a39c1a3109a7857ebf0318bc81bd0cf506f
Leondro Lio <github@leondrolio.com>
2020-10-01 12:14:22 +0100

    Create homepage for home components to live in

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { ComingSoon } from "../components/ComingSoon"
+import { Layout } from "../components/Layout"
+import { SEO } from "../components/SEO"
+
+const HomePage = () => (
+  <Layout>
+    <SEO title="Home" />
+    {/* Insert elements here! */}
+  </Layout>
+)
+
+export default HomePage
