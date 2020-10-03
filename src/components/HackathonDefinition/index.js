866e3dfdfb11bfe82170e590e8e792f9e051dbb2
Leondro Lio <github@leondrolio.com>
2020-10-03 23:28:25 +0100

    Shove hackathon definition to single column on mobile

--- src/components/HackathonDefinition/index.js
+++ src/components/HackathonDefinition/index.js
@@ -12,31 +12,33 @@ class HackathonDefinition extends Component {
           </div>
         </div>
         <div className="row justify-content-center py-5">
-          <div className="col-4 text-right">
-            <h4>
-              Royal Hackaway is an initiative of the{" "}
-              <a href="https://cmpsc.uk">
-                Computing Society at Royal Holloway, Univertiy of London
-              </a>
-            </h4>
-            <h4>
-              We aim to create great experiences for students of every
-              background of study by hosting inclusive events where people can
-              learn and grow.
-            </h4>
-          </div>
-          <div className="col-4">
-            <h1>hackathon</h1>
-            <h5 className="font-weight-light">
-              <i>/ˈhækəθɒn/</i>
-            </h5>
-            <h4 className="font-weight-light">
-              <i>
-                an event, typically lasting several days, in which a large
-                number of people meet to engage in collaborative computer
-                programming.
-              </i>
-            </h4>
+          <div className={CombineStyles("container", styles.lrContainer)}>
+            <div className={styles.left}>
+              <h4>
+                Royal Hackaway is an initiative of the{" "}
+                <a href="https://cmpsc.uk">
+                  Computing Society at Royal Holloway, Univertiy of London
+                </a>
+              </h4>
+              <h4>
+                We aim to create great experiences for students of every
+                background of study by hosting inclusive events where people can
+                learn and grow.
+              </h4>
+            </div>
+            <div className={styles.right}>
+              <h1>hackathon</h1>
+              <h5 className="font-weight-light">
+                <i>/ˈhækəθɒn/</i>
+              </h5>
+              <h4 className="font-weight-light">
+                <i>
+                  an event, typically lasting several days, in which a large
+                  number of people meet to engage in collaborative computer
+                  programming.
+                </i>
+              </h4>
+            </div>
           </div>
         </div>
       </section>
