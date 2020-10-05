874df36896f724e3f110c5b1694337be2ad9de09
TomAllison067 <tomlaurenceallison@gmail.com>
2020-10-05 18:07:27 +0100

    Added EventFAQ to home.js for my own reference while developing

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,13 +3,14 @@ import { Layout } from "../components/Layout"
 import { SEO } from "../components/SEO"
 import { HackathonDefinition } from "../components/HackathonDefinition"
 import { HackathonTitle } from "../components/HackathonTitle"
-
+import { EventFrequentlyAskedQuestions } from "../components/EventFrequentlyAskedQuestions"
 const HomePage = () => (
   <Layout>
     <SEO title="Home" />
     {/* Insert elements here! */}
     <HackathonTitle />
     <HackathonDefinition />
+    <EventFrequentlyAskedQuestions />
   </Layout>
 )
 
