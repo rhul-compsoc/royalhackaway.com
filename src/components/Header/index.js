039c6342ecc97c6877d82b290bacdf5053fd1be8
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2020-10-10 20:14:09 +0100

    Feature: adds a responsive navigation bar
    This commit brings the following changes:
    
    - Replaces the old navigation bar with a new one that is responsive
    on mobile devices.
    
    - Moves the MajorLeagueHackingBadge into the navigation bar component,
    that way it stays relative to the navigation bar and does not overlap
    the menu.
    
    - Removes the yarn.lock file and defaults into using npm throughout the
    project.
    
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   src/components/Header/index.js
    	modified:   src/components/Header/index.module.scss
    	modified:   src/components/Layout/index.js
    	modified:   src/components/MajorLeagueHackingBadge/index.module.scss
    	deleted:    yarn.lock

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,52 +1,52 @@
 import { Link } from "gatsby"
-import React, { Component } from "react"
+import React, { useState } from "react"
 import { CombineStyles } from "../../helpers/CombineStyles"
 import logo from "../../assets/images/logo/rh.svg"
 import styles from "./index.module.scss"
 import sponsorHandout from "../../assets/pdf/sponsorhandout.pdf"
+import { MajorLeagueHackingBadge } from "../MajorLeagueHackingBadge"
 
-class Header extends Component {
-  render() {
-    return (
-      <header className={styles.header}>
-        <nav className="navbar navbar-expand-lg bg-light fixed-top">
-          <div className="container">
-            <Link
-              className={CombineStyles("navbar-brand", styles.logoLink)}
-              to="/"
-            >
-              <img
-                src={logo}
-                className={CombineStyles(
-                  "d-inline-block",
-                  "align-top",
-                  styles.logo
-                )}
-                alt="RoyalHackaway"
-              />
-            </Link>
+export const Header = () => {
+  const [isActive, setIsActive] = useState(false)
 
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav">
-                <li>
-                  <Link to="/" className="nav-link text-dark">
-                    Home
-                  </Link>
-                </li>
-              </ul>
-              <ul className="navbar-nav">
-                <li>
-                  <a href={sponsorHandout} className="nav-link text-dark">
-                    Sponsor Us
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-    )
+  const toggleIsActive = () => {
+    setIsActive(prevState => !prevState)
   }
-}
 
-export { Header }
+  return (
+    <header>
+      <nav className={CombineStyles(styles.navBar, "container")}>
+        <div className={styles.navBarLeftSide}>
+          <button type="button" onClick={() => toggleIsActive()}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <Link to="/">
+            <img src={logo} alt="RoyalHackaway" />
+          </Link>
+        </div>
+        <div
+          className={
+            isActive
+              ? CombineStyles(
+                  styles.navBarRightSide,
+                  styles.navBarRightSideActive
+                )
+              : styles.navBarRightSide
+          }
+        >
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <a href={sponsorHandout}>Sponsor Us</a>
+            </li>
+          </ul>
+          <MajorLeagueHackingBadge />
+        </div>
+      </nav>
+    </header>
+  )
+}
