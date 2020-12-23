98595550dea3fae5c703d13b9886d862d9af53e0
Leondro Lio <github@leondrolio.com>
2020-12-23 15:05:14 +0000

    Add Google Analytics to the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,8 +51,13 @@ module.exports = {
         icon: "src/assets/images/logo/rh-small-1024.png", // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: [
+          "G-YHVZ8QBQYD"
+        ]
+      }
+    }
   ],
 }
