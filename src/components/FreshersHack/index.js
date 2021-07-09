5abf6933985d582de9bf742c19b81f5740da5ffa
Leondro Lio <github@leondrolio.com>
2021-07-09 19:13:44 +0100

    Add dark theme fresher's hack

--- src/components/FreshersHack/index.js
+++ src/components/FreshersHack/index.js
@@ -1,11 +1,20 @@
 import React from "react"
 import { background, letters } from "./index.module.scss"
 import letterImage from "./logo_var.png"
+import darkLetterImage from "./logo_var_orange.png"
+import { CombineStyles } from "../../helpers/CombineStyles"
 
 const FreshersHack = ({ style }) => {
   return (
     <div className={background} style={style}>
-      <img src={letterImage} className={letters} />
+      <img
+        src={letterImage}
+        className={CombineStyles(letters, "hackaway-light-theme-only")}
+      />
+      <img
+        src={darkLetterImage}
+        className={CombineStyles(letters, "hackaway-dark-theme-only")}
+      />
     </div>
   )
 }
