854540cafe6efb3d5d47d9ca03eba6fee6e419f4
Leondro Lio <github@leondrolio.com>
2021-09-06 23:27:07 +0100

    add a "get ticket" buddon

--- src/components/ButtonsContainer/index.tsx
+++ src/components/ButtonsContainer/index.tsx
@@ -1,8 +1,15 @@
 import React, { ReactNode } from "react"
+import { CombineStyles } from "../../helpers/CombineStyles"
 import * as styles from "./index.module.scss"
 
-const ButtonsContainer = ({ children }: { children: ReactNode }): ReactNode => (
-  <div className={styles.buttons}>{children}</div>
+const ButtonsContainer = ({
+  children,
+  className,
+}: {
+  children: ReactNode
+  className?: string
+}): ReactNode => (
+  <div className={CombineStyles(styles.buttons, className)}>{children}</div>
 )
 
 export { ButtonsContainer }
