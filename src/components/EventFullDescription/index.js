a42dd7467b9b20687e6619e02282f894ba77e2dc
Leondro Lio <github@leondrolio.com>
2021-12-20 17:39:12 +0000

    Let people know that v5 is a hybrid event

--- src/components/EventFullDescription/index.js
+++ src/components/EventFullDescription/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 import { EventContext } from "../EventContext"
+import { description as descriptionClass } from "./index.module.scss"
 
 const EventFullDescription = () => {
   const data = React.useContext(EventContext)
 
-  return <span>{data.mdx.frontmatter.full_description}</span>
+  return (
+    <span className={descriptionClass}>
+      {data.mdx.frontmatter.full_description}
+    </span>
+  )
 }
 
 export { EventFullDescription }
