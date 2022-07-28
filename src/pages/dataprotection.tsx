0a5ef67401ffb649bf634acb3a2f2fc8d293a964
JYewman <joshua.yewman@outlook.com>
2022-07-28 13:18:23 +0100

    Changes to be committed: 	modified:   src/pages/dataprotection.tsx     notes:      Complete dataprotection rewrite

--- src/pages/dataprotection.tsx
+++ src/pages/dataprotection.tsx
@@ -7,100 +7,91 @@ const DataProtectionPage = (): ReactNode => (
     <SiteSEO title="Data Protection Policy" />
     <section>
       <div className="container">
-        <h1>Data Protection Policy</h1>
-        <p>
-          We recognise our responsibility as an organisation collecting personal
-          data to handle that data in a way that is commensurate with the Data
-          Protection Act and our values as an organisation.
-        </p>
-        <p>The law states:</p>
+        <h1>Privacy Policy for Royal Hackaway (Royal Holloway Computing Soceity)</h1>
+
+        <p>At Royal Hackaway, accessible from royalhackaway.co.uk, one of our main priorities is the privacy of our visitors.
+          This Privacy Policy document contains types of information that is collected and recorded by Royal Hackaway and how we use it.</p>
+
+        <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us. </p>
+
+        <h2>General Data Protection Regulation (GDPR)</h2>
+        <p>We are a Data Controller of your information.</p>
+
+        <p>Royal Holloway Computing Society legal basis for collecting and using the personal information described in this
+          Privacy Policy depends on the Personal Information we collect and the specific context in which we collect the information:</p>
         <ul>
-          <li>Personal data shall be processed fairly and lawfully</li>
-          <li>
-            Personal data shall be obtained only for one or more specified and
-            lawful purposes, and shall not be further processed in any manner
-            incompatible with that purpose or purposes.
-          </li>
-          <li>
-            Personal data shall be adequate, relevant and not excessive in
-            relation to the purpose or purposes for which they are processed.
-          </li>
-          <li>
-            Personal data shall be accurate and, where necessary, kept up to
-            date.
-          </li>
-          <li>
-            Personal data processed for any purpose or purposes shall not be
-            kept for longer than is necessary for that purpose or those
-            purposes.
-          </li>
-          <li>
-            Personal data shall be processed in accordance with the rights of
-            data subjects (individuals).
-          </li>
-          <li>
-            Appropriate technical and organisational measures shall be taken
-            against unauthorised or unlawful processing of personal data and
-            against accidental loss or destruction of, or damage to, personal
-            data.
-          </li>
-          <li>
-            Personal data shall not be transferred to a country or territory
-            outside the European Economic Area unless that country or territory
-            ensures an adequate level of protection for the rights and freedoms
-            of data subjects in relation to the processing of personal data.
-          </li>
+          <li>Royal Holloway Computing Society needs to perform a contract with you</li>
+          <li>You have given Royal Holloway Computing Society permission to do so</li>
+          <li>Processing your personal information is in Royal Holloway Computing Society legitimate interests</li>
+          <li>Royal Holloway Computing Society needs to comply with the law</li>
         </ul>
-        <h2>We will</h2>
+
+        <p>Royal Holloway Computing Society will retain your personal information only for as long as is necessary for the purposes set out in this Privacy Policy.
+          We will retain and use your information to the extent necessary to comply with our legal obligations, resolve disputes, and enforce our policies.</p>
+
+        <p>If you are a resident of the European Economic Area (EEA), you have certain data protection rights.
+          If you wish to be informed what Personal Information we hold about you and if you want it to be removed from our systems, please contact us.</p>
+        <p>In certain circumstances, you have the following data protection rights:</p>
         <ul>
-          <li>
-            Only collect data that is absolutely necessary for us to run events,
-            safely, securely and in compliance with other legal obligations
-          </li>
-          <li>
-            Hold your data for a maximum of 3 days after the published event end
-            time. This allows us to resolve any issues within a reasonable time
-            frame
-          </li>
-          <li>
-            Only share personal data if absolutely necessary and only when
-            relevant.
-            <br />
-            For example:
-            <ul>
-              <li>
-                We will provide security with a list of everyone in the building
-                (names and next of kin) in case of a fire
-              </li>
-              <li>
-                In the case of a missing person or medical emergency, we will
-                pass on the information we have on you (including next of kin)
-                to the emergency services
-              </li>
-            </ul>
-          </li>
-          <li>
-            Securely store your data in a master spreadsheet that only
-            organisers have access to
-          </li>
-          <li>Anonymise the statistics which we may pass on to sponsors</li>
-          <li>
-            Provide MLH with attendee information so they can assign your
-            university points in the overall rankings
-          </li>
+          <li>The right to access, update or to delete the information we have on you.</li>
+          <li>The right of rectification.</li>
+          <li>The right to object.</li>
+          <li>The right of restriction.</li>
+          <li>The right to data portability</li>
+          <li>The right to withdraw consent</li>
         </ul>
-        <h2>We will not</h2>
-        <ul>
-          Pass on your data to any third party (except as listed above) without
-          your consent. This includes CVs, contact details, and other personal
-          information.
-        </ul>
-        <ul>
-          Sell your data to our sponsors. We provide sponsors with anonymised
-          statistics to show age/university/diversity breakdowns, but we{" "}
-          <strong>do not</strong> share any identifiable or personal data with
-          them.
-        </ul>
+
+        <h2>Log Files</h2>
+
+        <p>Royal Hackaway follows a standard procedure of using log files. These files log visitors when they visit websites.
+          All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses,
+          browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks.
+          These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends,
+          administering the site, tracking users' movement on the website, and gathering demographic information.</p>
+
+        <h2>Cookies and Web Beacons</h2>
+
+        <p>Like any other website, Royal Hackaway uses 'cookies'. These cookies are used to store information including visitors' preferences,
+          and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page
+          content based on visitors' browser type and/or other information.</p>
+
+        <h2>Privacy Policies</h2>
+
+        <p>You may consult this list to find the Privacy Policy for each of the advertising partners of Royal Hackaway.</p>
+
+        <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements
+          and links that appear on Royal Hackaway, which are sent directly to users' browser. They automatically receive your IP address when this occurs.
+          These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on
+          websites that you visit.</p>
+
+        <p>Note that Royal Hackaway has no access to or control over these cookies that are used by third-party advertisers.</p>
+
+        <h2>Third Party Privacy Policies</h2>
+
+        <p>Royal Hackaway's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective
+          Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out
+          of certain options. </p>
+
+        <p>You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with
+          specific web browsers, it can be found at the browsers' respective websites.</p>
+
+        <h2>Children's Information</h2>
+
+        <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe,
+          participate in, and/or monitor and guide their online activity.</p>
+
+        <p>Royal Hackaway does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided
+          this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove
+          such information from our records.</p>
+
+        <h2>Online Privacy Policy Only</h2>
+
+        <p>Our Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared
+          and/or collect in Royal Hackaway. This policy is not applicable to any information collected offline or via channels other than this website.</p>
+
+        <h2>Consent</h2>
+
+        <p>By using our website, you hereby consent to our Privacy Policy and agree to its terms.</p>
       </div>
     </section>
   </Layout>
