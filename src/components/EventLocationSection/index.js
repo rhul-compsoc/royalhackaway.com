9adb42a7054a3c6070107db1c43e8b08b89f04bd
Leo Durrant <poweredbykurisu@hotmail.com>
2022-08-02 15:11:23 +0100

    fix: styling issues

--- src/components/EventLocationSection/index.js
+++ src/components/EventLocationSection/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react"
+import { CombineStyles } from "../../helpers/CombineStyles"
 import { googleMaps } from "./index.module.scss"
 
 class EventLocationSection extends Component {
   render() {
     return (
-      <section className={googleMaps}>
+      <section className={CombineStyles(googleMaps, this.props.className)}>
         <iframe
           src={this.props.map}
           frameBorder="0"
